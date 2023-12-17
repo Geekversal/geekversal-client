0b5709d18a15849ba0ee1569fe93a4f8dacff510
Desmond <shuguang-lv@outlook.com>
2023-12-17 00:31:49 -0800

    build(ext): improve configs

--- vite.config.ts
+++ vite.config.ts
@@ -11,105 +11,117 @@ import Pages from 'vite-plugin-pages'
 import manifest from './manifest.config'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(new URL('./src', import.meta.url)),
-      src: fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  plugins: [
-    crx({ manifest }),
+export default defineConfig(({ mode }) => {
+  console.log(`run in the mode: ${mode}`)
 
-    vue(),
-
-    Pages({
-      dirs: [
-        {
-          dir: 'src/pages',
-          baseRoute: '',
-        },
-        {
-          dir: 'src/options/pages',
-          baseRoute: 'options',
-        },
-        {
-          dir: 'src/popup/pages',
-          baseRoute: 'popup',
-        },
-        {
-          dir: 'src/content-script/iframe/pages',
-          baseRoute: 'iframe',
-        },
-      ],
-    }),
-
-    AutoImport({
-      imports: ['vue', 'vue-router', 'vue/macros', '@vueuse/core'],
-      dts: 'src/auto-imports.d.ts',
-      dirs: ['src/composables/'],
-    }),
-
-    // https://github.com/antfu/unplugin-vue-components
-    Components({
-      dirs: ['src/components'],
-      // generate `components.d.ts` for ts support with Volar
-      dts: 'src/components.d.ts',
-      resolvers: [
-        // auto import icons
-        IconsResolver({
-          prefix: 'i',
-          enabledCollections: ['mdi'],
-        }),
-      ],
-    }),
-
-    // https://github.com/antfu/unplugin-icons
-    Icons({
-      autoInstall: true,
-      compiler: 'vue3',
-      scale: 1.5,
-    }),
-
-    // rewrite assets to use relative path
-    {
-      name: 'assets-rewrite',
-      enforce: 'post',
-      apply: 'build',
-      transformIndexHtml(html, { path }) {
-        return html.replace(
-          /"\/assets\//g,
-          `"${relative(dirname(path), '/assets')}/`
-        )
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(new URL('./src', import.meta.url)),
+        src: fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        iframe: 'src/content-script/iframe/index.html',
+    plugins: [
+      crx({ manifest }),
+
+      vue(),
+
+      Pages({
+        dirs: [
+          {
+            dir: 'src/pages',
+            baseRoute: '',
+          },
+          {
+            dir: 'src/options/pages',
+            baseRoute: 'options',
+          },
+          {
+            dir: 'src/popup/pages',
+            baseRoute: 'popup',
+          },
+          {
+            dir: 'src/content-script/iframe/pages',
+            baseRoute: 'iframe',
+          },
+        ],
+      }),
+
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'vue/macros',
+          '@vueuse/core',
+          {
+            'webextension-polyfill': [['*', 'browser']],
+          },
+        ],
+        dts: 'src/auto-imports.d.ts',
+        dirs: ['src/composables/'],
+      }),
+
+      // https://github.com/antfu/unplugin-vue-components
+      Components({
+        dirs: ['src/components'],
+        // generate `components.d.ts` for ts support with Volar
+        dts: 'src/components.d.ts',
+        resolvers: [
+          // auto import icons
+          IconsResolver({
+            prefix: 'i',
+            enabledCollections: ['mdi'],
+          }),
+        ],
+      }),
+
+      // https://github.com/antfu/unplugin-icons
+      Icons({
+        autoInstall: true,
+        compiler: 'vue3',
+        scale: 1.5,
+      }),
+
+      // rewrite assets to use relative path
+      {
+        name: 'assets-rewrite',
+        enforce: 'post',
+        apply: 'build',
+        transformIndexHtml(html, { path }) {
+          return html.replace(
+            /"\/assets\//g,
+            `"${relative(dirname(path), '/assets')}/`
+          )
+        },
+      },
+    ],
+    build: {
+      rollupOptions: {
+        input: {
+          iframe: 'src/content-script/iframe/index.html',
+        },
       },
     },
-  },
-  server: {
-    port: 8888,
-    strictPort: true,
-    hmr: {
-      port: 8889,
-      overlay: false,
-    },
-    proxy: {
-      "/api": {
-        target: process.env.VUE_APP_BASE_URL,
-        ws: true,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    server: {
+      port: 8888,
+      strictPort: true,
+      hmr: {
+        port: 8889,
+        overlay: false,
       },
-    }
-  },
-  optimizeDeps: {
-    include: ['vue', '@vueuse/core'],
-    exclude: ['vue-demi'],
-  },
+      proxy: {
+        '/api': {
+          target: process.env.VUE_APP_BASE_URL,
+          ws: true,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    optimizeDeps: {
+      include: ['vue', '@vueuse/core', 'webextension-polyfill'],
+      exclude: ['vue-demi'],
+    },
+  }
 })
